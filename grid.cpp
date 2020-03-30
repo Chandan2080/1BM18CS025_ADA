6f2ccc29f69783a14d0b94092580042b81d4caae
Chandan <36081499+Chandan2080@users.noreply.github.com>
2020-03-30 18:42:53 +0530

    Create grid.cpp

---
+++ grid.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void findGrid(int n){ 
+	int arr[n][n]; 
+
+	 
+	int x = 0; 
+
+	for (int i = 0; i < n / 4; i++) { 
+		for (int j = 0; j < n / 4; j++) { 
+			for (int k = 0; k < 4; k++) { 
+				for (int l = 0; l < 4; l++) { 
+					arr[i * 4 + k][j * 4 + l] = x; 
+					x++; 
+				} 
+			} 
+		} 
+	} 
+
+	 
+	for (int i = 0; i < n; i++) { 
+		for (int j = 0; j < n; j++) { 
+			cout << arr[i][j] << " "; 
+		} 
+		cout << "\n"; 
+	} 
+} 
+
+// Driver code 
+int main() 
+{ 
+    int n;
+    cout<<"Enter a value multiple of 4:\t";
+    cin>>n;
+	if(n%4==0)
+	    findGrid(n);
+	else
+        cout<<"Invalid value";
+	return 0; 
+}
