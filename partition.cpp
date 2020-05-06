ecd6a00caa2f3d9b155138e567fa1d777b927012
Chandan <36081499+Chandan2080@users.noreply.github.com>
2020-05-06 23:08:59 +0530

    Update partition.cpp

--- partition.cpp
+++ partition.cpp
@@ -1,1 +1,35 @@
-#include
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int n, int sum)
+{
+
+    if (sum == 0)
+        return 1;
+    else if (n == 0 )
+        return 0;
+    if(arr[n-1]>sum){
+        return 0;
+    }
+    return partition(arr, n-1, sum)||partition (arr, n-1, sum-arr[n-1]);
+}
+int main(){
+int sum = 0,n,arr[n];
+cout<<"Enter the number of elements: "<<endl;
+cin>>n;
+cout<<"Enter the elements :"<<endl;
+for (int i = 0; i < n; i++)
+    {
+    cin>>arr[i];
+    sum += arr[i];
+    }
+    if(sum%2==0 && partition(arr,n,sum/2)){
+
+            cout<<"Can be partitioned into two subsets of equal sum"<<endl;
+
+    }
+    else{
+        cout<<"Cannot be partitioned into two subsets of equal sum"<<endl;
+    }
+return 0;
+}
